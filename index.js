47e0fee884fceb654b185a54f7fa17520efe2a2a
Kelvin Au <kelvinau4413@hotmail.com>
2018-12-15 16:50:06 -0800

    add browsing to each tab for sharing more content

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const cred = require('./config.json');
 
 (async function() {
   const browser = await puppeteer.launch({
-    // headless: false,
+    //headless: false,
   });
   const page = await browser.newPage();
+
   await page.goto('https://amplify.fortinet.com');
 
   try {
@@ -28,63 +29,75 @@ const cred = require('./config.json');
     await loginPage.type('#id_password', cred.password);
     await loginPage.click(loginBtnSelector);
 
-
-    // Seems like there's a max. for this
-    const likeBtnSelector = '.action-bar__like-icon';
-    await page.waitFor(likeBtnSelector);
-    for (let i = 1; i <= 9; i++) {
-      await page.click(likeBtnSelector);
-      await page.waitFor(2000);
-      await page.click(likeBtnSelector);
-      await page.waitFor(2000);
-      console.log(`clicked ${i}`);
+    // because it's small screen, only top nav bar is shown
+    const tabBtnSelector =
+      '.nav.top-nav div.nav-item__child-ctn:not(.hidden-tab) li.nav-item';
+    await page.waitFor(tabBtnSelector);
+    const tabBtn = await page.$$(tabBtnSelector);
+    const tabIndex = [0, 1, 2, 3];
+    // go into each tab
+    for (let i = 0; i < tabIndex.length; i++) {
+      console.log(`going to tab ${tabIndex[i]}...`);
+      await tabBtn[tabIndex[i]].click();
+      await browseEachTab();
     }
 
-    // Don't want to reshare...
-    const shareBtnSelector = '.action-bar__share';
-    const linkSelector = '.modal-wrapper .nav-link';
-    const loaderSelector = '.loader-ctn';
-    const modalShareSelector = '.modal-wrapper .share__actions__button';
-
-    await page.waitFor(shareBtnSelector);
-    const shareBtn = await page.$$(shareBtnSelector);
-
-    for (let btn of shareBtn) {
-      // only check one post
-      await btn.click();
-      await page.waitFor(linkSelector);
-      const links = await page.$$(linkSelector);
-
-      await page.waitFor(5000);
-
-      await Promise.all([
-        page.click('.modal-wrapper .nav-link:not(.active)'),
-        // page.waitFor(loaderSelector),
-      ])
-
-      // await page.waitFor('.loader-ctn', {
-      // hidden: true,
-      // });
-
-      await page.waitFor(5000);
-
-      const notShared = await page.$('.loader-no-content');
-      if (notShared) {
-        console.log('sharing...');
-        await page.click('.modal-wrapper .nav-link:not(.active)');
-        await page.waitFor(modalShareSelector);
-        await page.click(modalShareSelector);
-        // wait for notification to go away
-        await page.waitFor(8000);
+    async function browseEachTab() {
+      // Seems like there's a max. for this
+      const likeBtnSelector = '.action-bar__like-icon';
+      await page.waitFor(likeBtnSelector);
+      for (let i = 1; i <= 9; i++) {
+        await page.click(likeBtnSelector);
+        await page.waitFor(2000);
+        await page.click(likeBtnSelector);
+        await page.waitFor(2000);
+        console.log(`clicked ${i}`);
       }
-      else {
-        console.log('shared already');
-        await page.click('.modal-wrapper svg.panel__header__close__icon');
+
+      // Don't want to reshare...
+      const shareBtnSelector = '.action-bar__share';
+      const linkSelector = '.modal-wrapper .nav-link';
+      const loaderSelector = '.loader-ctn';
+      const modalShareSelector = '.modal-wrapper .share__actions__button';
+
+      await page.waitFor(shareBtnSelector);
+      const shareBtn = await page.$$(shareBtnSelector);
+
+      for (let btn of shareBtn) {
+        // only check one post
+        await btn.click();
+        await page.waitFor(linkSelector);
+        const links = await page.$$(linkSelector);
+
+        await page.waitFor(5000);
+
+        await Promise.all([
+          page.click('.modal-wrapper .nav-link:not(.active)'),
+          // page.waitFor(loaderSelector),
+        ]);
+
+        // await page.waitFor('.loader-ctn', {
+        // hidden: true,
+        // });
+
+        await page.waitFor(5000);
+
+        const notShared = await page.$('.loader-no-content');
+        if (notShared) {
+          console.log('sharing...');
+          await page.click('.modal-wrapper .nav-link:not(.active)');
+          await page.waitFor(modalShareSelector);
+          await page.click(modalShareSelector);
+          // wait for notification to go away
+          await page.waitFor(8000);
+        } else {
+          console.log('shared already');
+          await page.click('.modal-wrapper svg.panel__header__close__icon');
+        }
+        await page.waitFor(2000);
       }
-      await page.waitFor(2000);
     }
-  }
-  catch(e) {
+  } catch (e) {
     console.log(e);
   }
 
